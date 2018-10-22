7bf04c5a364a4f9d38c251968792b547b5b10062
GedeonBashimbe <gedeonb1015@gmail.com>
2018-10-22 15:14:29 -0500

    Created day 1 classes and worked with arrays

---
+++ Project/src/com/fundamentals/java/Cat.java
@@ -0,0 +1,19 @@
+package com.fundamentals.java;
+
+public class Cat {
+	
+	public String fourlegs;
+	
+	public String fur;
+	
+	public String whiskers;
+	
+	public void jump() {
+		
+	}
+	
+	public void scratch() {
+		
+	}
+
+}
---
+++ Project/src/com/fundamentals/java/House.java
@@ -0,0 +1,19 @@
+package com.fundamentals.java;
+
+public class House {
+	
+	public String windows;
+	
+	public String doors;
+	
+	public boolean hasStairs;
+	
+	public void openDoor() {
+		
+	}
+	
+	public void closeDoor() {
+		
+	}
+
+}
